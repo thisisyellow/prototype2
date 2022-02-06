45699a16988145fba980058c724a5ef5b16d493c
thisisyellow <73936439+thisisyellow@users.noreply.github.com>
2022-02-06 13:24:24 +0200

    Add files via upload

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public bool isReadyForInstantiate;
+
+    void Start()
+    {
+        isReadyForInstantiate = true;
+    }
+
+    void Update()
+    {
+        if (isReadyForInstantiate && Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(PreventSpam());
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+
+    IEnumerator PreventSpam()
+    {
+        isReadyForInstantiate = false;
+        yield return new WaitForSeconds(2);
+        isReadyForInstantiate = true;
+    }
+}
